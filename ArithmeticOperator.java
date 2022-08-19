3f8500d3d6e17646c5489fc87316e338a784bfa8
first <romanrajesh3977@gmail.com>
2022-08-19 16:56:53 +0530

    arthimetic

---
+++ ArithmeticOperator.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class ArithmeticOperator {
+	public static void main(String[] args)
+	{
+		int number1=20;
+		int number2=15;
+		int result1=number1+number2;
+		int result2=number1-number2;
+		int result3=number1*number2;
+		int result4=number1/number2;
+		int result5=number1%number2;
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+		
+		
+	}
+}
